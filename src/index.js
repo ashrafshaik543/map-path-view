44ec630e54b4c568286875f3237e4d6112a884cd
ash543210 <shaikashraf54321@gmail.com>
2024-02-10 00:27:59 +0530

    Revert "first commit"
    This reverts commit 3f7b84d5633cb86eee032bd1282d987a59cf1718.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import DisplayRoutes from "./components/DisplayRoutes";
-import "mapbox-gl/dist/mapbox-gl.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-
-const router = createBrowserRouter([
-  {
-    path: "/route",
-    element: <DisplayRoutes />,
-  },
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-
-    {/* <App /> */}
-    {/* <DisplayRoutes /> */}
+    <App />
   </React.StrictMode>
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <DisplayRoutes />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
